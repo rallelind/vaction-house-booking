dc8dec8a0d02509e246b0f993aa38feaaa1291b7
Rasmus Lind <rasmus@voiceboxer.com>
2023-06-01 19:57:59 +0200

    make sure user is logged in

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,9 +1,24 @@
 import useSWR from "swr";
+import { useRouter } from "next/navigation";
 
-const fetcher = (url: string) => fetch(url, { method: 'GET', credentials: 'include' }).then(r => r.json())
+const fetcher = async (url: string) => {
+    const response = await fetch(url, { method: 'GET', credentials: 'include' });
+
+    if(response.status === 401) {
+       throw new Error("unauthorized")
+    }
+
+    return response.json()
+}
 
 export default function useUser() {
     const { data, error } = useSWR("http://localhost:3000/users/me", fetcher)
+
+    const router = useRouter();
+
+    if(error) {
+        router.push("/sign-in")
+    }
 
     return {
         user: data,
