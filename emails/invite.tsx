d683ef2d5e7424f1b016a3423d12365b1d87f440
Rasmus Lind <rasmus@voiceboxer.com>
2023-07-09 23:11:45 +0200

    send email

---
+++ emails/invite.tsx
@@ -0,0 +1,76 @@
+import {
+  Tailwind,
+  Html,
+  Button,
+  Preview,
+  Body,
+  Container,
+  Section,
+  Heading,
+  Text,
+  Link,
+} from "@react-email/components";
+import * as React from "react";
+import Avatar from "../src/components/ui/Avatar";
+import {
+  ArrowSmallRightIcon,
+  EnvelopeIcon,
+  HomeIcon,
+} from "@heroicons/react/24/outline";
+
+export default function Invite({
+  preview = "Join havklitvej 60",
+  houseName = "Havklitvej 60",
+  recipient = "example@example.com",
+  houseInviteLink = "",
+  user = { name: "ralle", email: "rasmus1234555@hotmail.dk", picture: "" },
+}) {
+  const { name, email, picture } = user;
+
+  return (
+    <Html>
+      <Preview>{preview}</Preview>
+      <Tailwind>
+        <Body className="bg-white my-auto mx-auto font-sans">
+          <Container className="border border-solid border-gray-300 mx-auto rounded-lg p-4 mt-6">
+            <Heading className="text-center font-normal text-3xl">
+              Du er inviteret til at være en del af{" "}
+              <strong className="text-orange-300">{houseName}</strong>
+            </Heading>
+            <Text>
+              Hejsa <strong>{recipient}</strong>
+            </Text>
+            <Text>
+              Du er inviteret af <strong>{name}</strong> (
+              <Link
+                href={`mailto:${email}`}
+                className="text-orange-300 font-semibold no-underline"
+              >
+                {email}
+              </Link>
+              ) til at blive medlem af huset Havklitvej 60.
+            </Text>
+            <div className="flex items-center justify-center">
+              <Avatar />
+              <ArrowSmallRightIcon className="h-6 mr-4 ml-4" />
+              <div className="bg-orange-200 rounded-lg h-10 w-10 flex items-center justify-center">
+                <HomeIcon className="h-6" />
+              </div>
+            </div>
+            <div className="mt-10 text-center">
+              <Button
+                className="bg-orange-100 p-2 rounded-lg border-solid border-[1px] border-orange-200 text-black"
+                href={houseInviteLink}
+              >
+                <div className="flex items-center h-6">
+                  <EnvelopeIcon className="h-6 mr-2" />
+                  <p>Bliv del af {houseName}</p>
+                </div>
+              </Button>
+            </div>
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  );
+}
--- src/app/houses/[id]/page.tsx
+++ src/app/houses/[id]/page.tsx
@@ -52,41 +52,47 @@ export default function HouseCreation() {
       });
   }, []);
 
+  console.log(predictions);
+
   return (
     <div>
-      <div className="relative">
-        <div id="map" className="h-96 rounded-lg"></div>
+      <div className="relative w-[600px] m-auto">
+        <div id="map" className="h-[500px] rounded-lg"></div>
         <div className="absolute top-0 p-6 w-full">
           <Combobox>
-            <div
-              className={`flex ${
-                predictions.length === 0 ? "rounded-lg" : "rounded-t-lg"
-              } p-4 w-full bg-white cursor-text`}
-            >
-              <div className="bg-orange-100 border-orange-200 p-2 rounded-lg border">
-                <MapPinIcon className="h-6" />
-              </div>
-              <Combobox.Input
-                className="focus:outline-none w-full ml-4"
-                onChange={(e) => setInputQuery(e.target.value)}
-              />
-            </div>
-            <Combobox.Options
-              className={`bg-white rounded-b-lg max-h-60 overflow-auto`}
-            >
-              {predictions.map((prediction) => (
-                <Combobox.Option
-                  className="p-4 flex items-center hover:bg-orange-50"
-                  value={prediction.description}
-                  key={prediction.place_id}
+            {({ open }) => (
+              <>
+                <div
+                  className={`flex ${
+                    !open ? "rounded-lg" : "rounded-t-lg"
+                  } p-4 w-full bg-white cursor-text`}
                 >
-                  <div className="bg-orange-100 rounded-lg p-2 border-orange-200 border">
-                    <HomeIcon className="h-6" />
+                  <div className="bg-orange-100 border-orange-200 p-2 rounded-lg border">
+                    <MapPinIcon className="h-6" />
                   </div>
-                  <p className="ml-4">{prediction.description}</p>
-                </Combobox.Option>
-              ))}
-            </Combobox.Options>
+                  <Combobox.Input
+                    className="focus:outline-none w-full ml-4"
+                    onChange={(e) => setInputQuery(e.target.value)}
+                  />
+                </div>
+                <Combobox.Options
+                  className={`bg-white rounded-b-lg max-h-60 overflow-auto`}
+                >
+                  {predictions.map((prediction) => (
+                    <Combobox.Option
+                      className="p-4 flex items-center hover:bg-orange-50"
+                      value={prediction.description}
+                      key={prediction.place_id}
+                    >
+                      <div className="bg-orange-100 rounded-lg p-2 border-orange-200 border">
+                        <HomeIcon className="h-6" />
+                      </div>
+                      <p className="ml-4">{prediction.description}</p>
+                    </Combobox.Option>
+                  ))}
+                </Combobox.Options>
+              </>
+            )}
           </Combobox>
         </div>
       </div>
